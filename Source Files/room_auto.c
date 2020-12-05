2fedb4b5abf157e9caf50831257d0da286e579af
debjyoti <debjyoti@rapidken.ai>
2020-12-05 19:05:58 +0530

    IOT Usart recieve  added

--- Source Files/room_auto.c
+++ Source Files/room_auto.c
@@ -11,18 +11,24 @@
 #define door_outer_IR RB0
 #define door_inner_IR RB1
 #define bathroom_door_IR RB2
-
+#define bathroom_light RC2
+#define room_light RA0
 
 
 #define _XTAL_FREQ 20000000
 #define FOSC 20000000L                      // Using External crystal of 20 MHz
 
-unsigned char person_enter_room,person_exit_room,bathroom_light,count=0,used_inner=0,used_outer=0,inner_used_already=0,outer_used_already=0,hour;
+unsigned char person_enter_room,person_exit_room,bathroom_light_var,count=0,used_inner=0,used_outer=0,inner_used_already=0,outer_used_already=0,hour,rcif_val,is_usart_recieved,rc_val_char;
 
 
 //Interrupts
 void __interrupt () my_isr_routine (void)
 {
+    if (RCIF==1)
+    {   
+        is_usart_recieved=1;
+        rcif_val=RCREG;
+    }
     if(RBIF==1)
     {
         RBIF=0;
@@ -45,7 +51,7 @@ void __interrupt () my_isr_routine (void)
         if (bathroom_door_IR==0)    
         {
             bathroom_door_IR=1;    
-            bathroom_light=1;
+            bathroom_light_var=1;
         }
     }
 }
@@ -83,7 +89,7 @@ void main()
             count++;
             if (count==1)
             {
-               USART_Sendstring("light_on"); 
+               room_light=1;
             }
         }
         if (person_exit_room==1)
@@ -93,7 +99,7 @@ void main()
             count--;
             if (count==0)
             {
-               USART_Sendstring("light_off"); 
+               room_light=0; 
             }
         }
         if (outer_used_already==1)
@@ -102,7 +108,7 @@ void main()
             count--;
             if (count==0)
             {
-               USART_Sendstring("light_off"); 
+               room_light=0; 
             }
         }
         if (inner_used_already==1)
@@ -111,13 +117,13 @@ void main()
             count++;
             if (count==1)
             {
-               USART_Sendstring("light_on"); 
+               room_light=1; 
             }
         }
 
-        if(bathroom_light==1)
+        if(bathroom_light_var==1)
         {   
-            bathroom_light=0;
+            bathroom_light_var=0;
             hour=i2c_receive();
             if (hour>0 && hour<6)
             {
@@ -127,11 +133,27 @@ void main()
             {
                 CCPR1L=0b11111010;
             }
-            RC2=!RC2;            
+            bathroom_light=!bathroom_light;            
+        }
+        if (is_usart_recieved == 1)
+        {
+            is_usart_recieved == 1;
+            rc_val_char=char(rc_val);
+            if (rc_val=='A')
+            {
+
+            }
+            if (rc_val=='B')
+            {
+                
+            }
+            if (rc_val=='C')
+            {
+                
+            }
         }
     }
 }
-
 
 
 
